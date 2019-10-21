c256dc77d3546f75b6953a885509dddfbc86e583
Chris Graham <chris@ocproducts.com>
2019-10-21 12:08:17 -0500

    Misc fixes

--- _tests/tests/unit_tests/ssl.php
+++ _tests/tests/unit_tests/ssl.php
@@ -23,6 +23,8 @@ class ssl_test_set extends cms_test_case
         parent::setUp();
 
         set_value('disable_ssl_for__' . $_SERVER['HTTP_HOST'], '1');
+
+        $this->establish_admin_session();
     }
 
     public function testHTTPSStatus()
@@ -59,7 +61,7 @@ class ssl_test_set extends cms_test_case
         $HTTPS_PAGES_CACHE = null;
         erase_persistent_cache();
         $url = build_url(array('page' => $page), get_module_zone($page));
-        $c = http_get_contents($url->evaluate(), array('convert_to_internal_encoding' => true, 'timeout' => 20.0));
+        $c = http_get_contents($url->evaluate(), array('cookies' => array(get_session_cookie() => get_session_id()), 'convert_to_internal_encoding' => true, 'timeout' => 20.0));
         $this->assertTrue(strpos($c, 'src="http://') === false, 'HTTPS version failed (HTTP embed [e.g. image] found) on ' . $url->evaluate());
 
         // HTTP version
@@ -67,7 +69,7 @@ class ssl_test_set extends cms_test_case
         $HTTPS_PAGES_CACHE = null;
         erase_persistent_cache();
         $url = build_url(array('page' => $page), get_module_zone($page));
-        $c = http_get_contents($url->evaluate(), array('convert_to_internal_encoding' => true, 'timeout' => 20.0));
+        $c = http_get_contents($url->evaluate(), array('cookies' => array(get_session_cookie() => get_session_id()), 'convert_to_internal_encoding' => true, 'timeout' => 20.0));
         $this->assertTrue(strpos($c, 'src="https://') === false, 'HTTP version failed (HTTPS embed [e.g. image] found) on ' . $url->evaluate());
     }
 
