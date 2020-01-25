78e2ca97d3eb7d52f878ca053e51913134d5c5da
Chris Graham <chrisgraham@Chriss-MacBook-Pro.local>
2020-01-24 20:31:50 -0600

    Fix assumptions about CodeSniffer being on

--- _tests/cmstest/cms_test_case.php
+++ _tests/cmstest/cms_test_case.php
@@ -78,7 +78,9 @@ class cms_test_case extends WebTestCase
     public function extend_cqc_call($url)
     {
         $url .= '&api=1&todo=1';
-        $url .= '&codesniffer=1';
+        if (strpos(shell_exec('npx eslint -v'), 'v') !== false) {
+            $url .= '&codesniffer=1';
+        }
         $url .= '&base_path=' . urlencode(get_file_base());
         if ($this->debug) {
             $url .= '&manual_checks=1&pedantic=1&security=1&mixed=1';
