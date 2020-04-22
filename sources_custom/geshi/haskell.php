978b0deb2024fd05a2932c062b8b2d8bfe743949
Chris Graham <chris@ocproducts.com>
2020-04-21 19:42:32 -0500

    Updated GeSHI (turns out it is maintained

--- sources_custom/geshi/haskell.php
+++ sources_custom/geshi/haskell.php
@@ -6,7 +6,7 @@
  *         Based on haskell.php by Jason Dagit (dagit@codersbase.com), which was
  *         based on ocaml.php by Flaie (fireflaie@gmail.com).
  * Copyright: (c) 2005 Flaie, Nigel McNie (http://qbnz.com/highlighter)
- * Release Version: 1.0.8.12
+ * Release Version: 1.0.9.1
  * Date Started: 2014/05/12
  *
  * Haskell language file for GeSHi.
@@ -40,7 +40,7 @@ $language_data = array (
         3 => "/{-(?:(?R)|.)-}/s", //Nested Comments
         ),
     'CASE_KEYWORDS' => 0,
-    'QUOTEMARKS' => array('"',"'"),
+    'QUOTEMARKS' => array('"'),
     'ESCAPE_CHAR' => '\\',
     'KEYWORDS' => array(
         /* main haskell keywords */
@@ -105,7 +105,7 @@ $language_data = array (
         4 => array (
             'Bool', 'Maybe', 'Either', 'Ordering',
             'Char', 'String',
-            'Int', 'Integer', 'Float', 'Double', 'Rational',
+            'Int', 'Integer', 'Float', 'Double', 'Rational', 'Word',
             'ShowS', 'ReadS',
             'IO', 'IOError', 'IOException'
             ),
@@ -114,7 +114,9 @@ $language_data = array (
             'Ord', 'Eq', 'Enum', 'Bounded',
             'Num', 'Real', 'Integral', 'Fractional',
             'Floating', 'RealFrac', 'RealFloat',
-            'Monad', 'Functor',
+            'Semigroup', 'Monoid',
+            'Monad', 'Applicative', 'Functor',
+            'Foldable', 'Traversable',
             'Show', 'Read'
             )
         ),
@@ -152,7 +154,7 @@ $language_data = array (
             0 => 'color: green;'
             ),
         'STRINGS' => array(
-            0 => 'background-color: #3cb371;' /* nice green */
+            0 => 'color: #3cb371;' /* nice green */
             ),
         'NUMBERS' => array(
             0 => 'color: red;' /* pink */
@@ -172,13 +174,13 @@ $language_data = array (
         /* some of keywords are Prelude functions */
         1 => '',
         /* link to the wanted library */
-        2 => 'http://haskell.org/ghc/docs/latest/html/libraries/base/{FNAME}.html',
+        2 => 'http://hackage.haskell.org/package/base/docs/{FNAME}.html',
         /* link to Prelude functions */
-        3 => 'http://haskell.org/ghc/docs/latest/html/libraries/base/Prelude.html#v:{FNAME}',
+        3 => 'http://hackage.haskell.org/package/base/docs/Prelude.html#v:{FNAME}',
         /* link to Prelude types */
-        4 => 'http://haskell.org/ghc/docs/latest/html/libraries/base/Prelude.html#t:{FNAME}',
+        4 => 'http://hackage.haskell.org/package/base/docs/Prelude.html#t:{FNAME}',
         /* link to Prelude exceptions */
-        5 => 'http://haskell.org/ghc/docs/latest/html/libraries/base/Prelude.html#t:{FNAME}'
+        5 => 'http://hackage.haskell.org/package/base/docs/Prelude.html#t:{FNAME}'
         ),
     'OOLANG' => false,
     'OBJECT_SPLITTERS' => array(
