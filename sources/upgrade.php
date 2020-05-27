d8acf40d119add4e82ffaed8f1376b8e78face89
Chris Graham <chris@ocproducts.com>
2020-05-27 11:08:09 -0500

    Implemented MANTIS-3013

--- sources/upgrade.php
+++ sources/upgrade.php
@@ -364,7 +364,7 @@ function upgrader_output_login($message = null)
 function upgrader_output_header()
 {
     $upgrader_title = do_lang('UPGRADER_UPGRADER_TITLE');
-    $upgrader_intro = do_lang('UPGRADER_UPGRADER_INTRO');
+    $upgrader_intro = do_lang('UPGRADER_UPGRADER_INTRO', escape_html(static_evaluate_tempcode(build_url(['page' => ''], 'adminzone'))));
     $charset = get_charset();
     $lang = user_lang();
     $dir = do_lang('dir');
