a276c69ba2820cbfbf8767bc13f814504b59a1f5
Chris Graham <chris@ocproducts.com>
2020-06-11 12:20:34 -0500

    Fixed MANTIS-4271 (Points not assigned for blog posts)

--- sources/submit.php
+++ sources/submit.php
@@ -147,6 +147,11 @@ function give_submit_points($type, $member = null)
         $member = get_member();
     }
     if ((!is_guest($member)) && (addon_installed('points'))) {
+        // FUDGE
+        if ($type == 'ADD_NEWS_BLOG') {
+            $type = 'ADD_NEWS';
+        }
+
         $points = get_option('points_' . $type, true);
         if (is_null($points)) {
             return '';
