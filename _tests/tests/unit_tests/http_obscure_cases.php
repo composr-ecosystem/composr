06028c8402ba9eab2de4c655d6f9978f39e7f0e1
Chris Graham <chris@ocproducts.com>
2020-07-02 15:26:18 -0500

    Fixed MANTIS-4307 (whoops original fix did not actually have fix; plus improve debugging within test)

--- _tests/tests/unit_tests/http_obscure_cases.php
+++ _tests/tests/unit_tests/http_obscure_cases.php
@@ -21,23 +21,34 @@ class http_obscure_cases_test_set extends cms_test_case
     // COR is very easy to accidentally break due to it running early in bootstrap and not being used much
     public function testCOR()
     {
+        global $HTTP_HEADERS;
+
         $extra_headers = array('Origin' => 'http://example.com');
         $http_verb = 'GET';
-        $test = http_download_file(get_base_url() . '/index.php', null, true, false, 'Composr', null, null, null, null, null, null, null, null, 6.0, false, null, $extra_headers, $http_verb);
+        $test = http_download_file(static_evaluate_tempcode(build_url(array('page' => ''), '')), null, true, false, 'Composr', null, null, null, null, null, null, null, null, 6.0, false, null, $extra_headers, $http_verb);
+        if (!empty($_GET['debug'])) { // TODO: Change in v11
+            var_dump($HTTP_HEADERS);
+        }
         $this->assertTrue($test != '');
         $this->assertTrue(!$this->hasCORHeader('Access-Control-Allow-Origin'));
         $this->assertTrue(!$this->hasCORHeader('Access-Control-Allow-Credentials'));
 
         $extra_headers = array('Origin' => get_base_url());
         $http_verb = 'GET';
-        $test = http_download_file(get_base_url() . '/index.php', null, true, false, 'Composr', null, null, null, null, null, null, null, null, 6.0, false, null, $extra_headers, $http_verb);
+        $test = http_download_file(static_evaluate_tempcode(build_url(array('page' => ''), '')), null, true, false, 'Composr', null, null, null, null, null, null, null, null, 6.0, false, null, $extra_headers, $http_verb);
+        if (!empty($_GET['debug'])) { // TODO: Change in v11
+            var_dump($HTTP_HEADERS);
+        }
         $this->assertTrue($test != '');
         $this->assertTrue($this->hasCORHeader('Access-Control-Allow-Origin'));
         $this->assertTrue(!$this->hasCORHeader('Access-Control-Allow-Credentials'));
 
         $extra_headers = array('Origin' => get_base_url());
         $http_verb = 'OPTIONS';
-        $test = http_download_file(get_base_url() . '/index.php', null, true, false, 'Composr', null, null, null, null, null, null, null, null, 6.0, false, null, $extra_headers, $http_verb);
+        $test = http_download_file(static_evaluate_tempcode(build_url(array('page' => ''), '')), null, false, false, 'Composr', null, null, null, null, null, null, null, null, 6.0, false, null, $extra_headers, $http_verb);
+        if (!empty($_GET['debug'])) { // TODO: Change in v11
+            var_dump($HTTP_HEADERS);
+        }
         $this->assertTrue($test == '');
         $this->assertTrue($this->hasCORHeader('Access-Control-Allow-Origin'));
         $this->assertTrue($this->hasCORHeader('Access-Control-Allow-Credentials'));
