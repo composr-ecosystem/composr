1214aa23eed9e4f3f8e69a948db85b62d0ea50f6
Chris Graham <chris@ocproducts.com>
2020-08-05 23:54:32 -0500

    Set HTTPS port when detecting forwarding proxy

--- sources/global.php
+++ sources/global.php
@@ -879,6 +879,7 @@ if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
                 }
                 if ((isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) && ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) {
                     $_SERVER['HTTPS'] = 'on';
+                    $_SERVER['SERVER_PORT'] = '443';
                 }
                 break;
             }
