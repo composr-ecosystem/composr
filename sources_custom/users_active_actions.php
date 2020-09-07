2de179b083f606d4c2d5206b638ac9c677d3c54e
Chris Graham <chris@ocproducts.com>
2020-09-07 13:02:21 -0500

    Hybridauth logout integration

---
+++ sources_custom/users_active_actions.php
@@ -0,0 +1,51 @@
+<?php /*
+
+ Composr
+ Copyright (c) ocProducts, 2004-2020
+
+ See docs/LICENSE.md for full licensing information.
+
+*/
+
+/**
+ * @license    http://opensource.org/licenses/cpal_1.0 Common Public Attribution License
+ * @copyright  ocProducts Ltd
+ * @package    hybridauth
+ */
+
+/**
+ * Process a logout.
+ */
+function handle_active_logout()
+{
+    non_overridden__handle_active_logout();
+
+    if (addon_installed('hybridauth')) {
+        // Log out of Hybridauth too...
+
+        require_code('hybridauth');
+
+        $before_type_strictness = ini_get('ocproducts.type_strictness');
+        cms_ini_set('ocproducts.type_strictness', '0');
+        $before_xss_detect = ini_get('ocproducts.xss_detect');
+        cms_ini_set('ocproducts.xss_detect', '0');
+
+        initiate_hybridauth_session_state();
+
+        $hybridauth = initiate_hybridauth();
+
+        if (isset($_SESSION['provider'])) {
+            $provider = $_SESSION['provider'];
+
+            try {
+                $adapter = $hybridauth->getAdapter($provider);
+                $adapter->disconnect();
+            } catch (Hybridauth\Exception $e) {
+                // Silent failure
+            }
+        }
+
+        cms_ini_set('ocproducts.type_strictness', $before_type_strictness);
+        cms_ini_set('ocproducts.xss_detect', $before_xss_detect);
+    }
+}
