9810af856048eafb98b340f7ac1528bbe6edd645
Chris Graham <chris@ocproducts.com>
2020-10-14 08:32:25 -0500

    Fixed MANTIS-4433 (Add search unit testing)

--- _tests/tests/unit_tests/search.php
+++ _tests/tests/unit_tests/search.php
@@ -27,6 +27,66 @@ class search_test_set extends cms_test_case
         require_code('xml');
     }
 
+    public function testNoSearchHooksCrash()
+    {
+        require_code('database_search');
+
+        $boolean_operator = 'AND';
+        $content = 'qwertyuiop';
+        list($content_where) = build_content_where($content, false, $boolean_operator);
+        $only_search_meta = false;
+        $direction = 'DESC';
+        $max = 10;
+        $start = 0;
+        $only_titles = false;
+        $author = '';
+        $author_id = null;
+        $cutoff = null;
+        $sort = 'relevance';
+        $where_clause = '';
+        $search_under = '!';
+        $boolean_search = false;
+
+        $_hooks = find_all_hooks('modules', 'search'); // TODO: find_all_hook_obs in v11
+        foreach (array_keys($_hooks) as $hook) {
+            require_code('hooks/modules/search/' . filter_naughty_harsh($hook));
+            $ob = object_factory('Hook_search_' . filter_naughty_harsh($hook), true);
+            if ($ob === null) {
+                continue;
+            }
+            $info = $ob->info();
+            if (($info === null) || ($info === false)) {
+                continue;
+            }
+
+            if (php_function_allowed('set_time_limit')) {
+                @set_time_limit(10); // Prevent errant search hooks (easily written!) taking down a server. Each call given 10 seconds (calling set_time_limit resets the timer).
+            }
+            $hook_results = $ob->run($content, $only_search_meta, $direction, $max, $start, $only_titles, $content_where, $author, $author_id, $cutoff, $sort, $max, $boolean_operator, $where_clause, $search_under, $boolean_search ? 1 : 0);
+            $this->assertTrue(is_array($hook_results));
+        }
+    }
+
+    public function testHttpGuestFullSearch()
+    {
+        require_lang('search');
+
+        $url_parts = array('page' => 'search', 'type' => 'results', 'content' => 'qwertyuiopxxx');
+
+        $_hooks = find_all_hooks('modules', 'search');
+        foreach (array_keys($_hooks) as $hook) {
+            $url_parts['search_' . $hook] = 1;
+        }
+
+        foreach (array(0, 1) as $safe_mode) {
+            foreach (array('admin', 'test') as $username) {
+                $url = build_url($url_parts + array('keep_su' => $username, 'keep_safe_mode' => $safe_mode), get_module_zone('search'));
+                $data = http_download_file($url->evaluate(), null, true, false, 'Composr', null, array(get_session_cookie() => get_session_id()));
+                $this->assertTrue(strpos($data, do_lang('NO_RESULTS_SEARCH')) !== false); // We expect no results, but also no crash!
+            }
+        }
+    }
+
     public function testOpenSearch()
     {
         $url = find_script('opensearch');
