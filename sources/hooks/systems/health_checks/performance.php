76ffa48d5abe345e78f624c9a1a87290ff2b1b00
Chris Graham <chris@ocproducts.com>
2020-11-16 20:16:38 -0600

    Whoops

--- sources/hooks/systems/health_checks/performance.php
+++ sources/hooks/systems/health_checks/performance.php
@@ -487,6 +487,6 @@ class Hook_health_check_performance extends Hook_Health_Check
         // LEGACY
         $this->assertTrue(cms_ini_get('xdebug.profiler_enable') !== '1', 'Xdebug (v2) profiler was left enabled, use xdebug.profiler_enable_trigger and xdebug.profiler_enable_trigger_value to selectively enable profiling if you want it');
         $this->assertTrue((@strval(ini_get('xdebug.start_with_request')) != 'yes') || (strpos(@strval(ini_get('xdebug.mode')), 'profile') === false), 'Xdebug (v3) profiler was left enabled, use xdebug.start_with_request=trigger and xdebug.trigger_value to selectively enable profiling if you want it');
-        $this->assertTrue((@strval(ini_get('xdebug.start_with_request')) != 'yes') || (strpos(@strval(ini_get('xdebug.mode')), 'coverage') === false(, 'Xdebug (v3) coverage mode was left enabled');
+        $this->assertTrue((@strval(ini_get('xdebug.start_with_request')) != 'yes') || (strpos(@strval(ini_get('xdebug.mode')), 'coverage') === false), 'Xdebug (v3) coverage mode was left enabled');
     }
 }
