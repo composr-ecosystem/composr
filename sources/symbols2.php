8aeb7c2709600181bdaccab82a8aa5b481ec8220
Chris Graham <chris@ocproducts.com>
2021-11-02 12:18:20 -0500

    New KEEP_POST symbol

--- sources/symbols2.php
+++ sources/symbols2.php
@@ -2595,6 +2595,29 @@ function ecv2_THEME_DARK(string $lang, array $escaped, array $param) : string
  * @param  array $param Parameters to the symbol. For all but directive it is an array of strings. For directives it is an array of Tempcode objects. Actually there may be template-style parameters in here, as an influence of singular_bind and these may be Tempcode, but we ignore them.
  * @return string The result
  */
+function ecv2_KEEP_POST(string $lang, array $escaped, array $param) : string
+{
+    $value = build_keep_post_fields($param);
+    if ($GLOBALS['XSS_DETECT']) {
+        ocp_mark_as_escaped($value);
+    }
+
+    if (!empty($escaped)) {
+        apply_tempcode_escaping($escaped, $value);
+    }
+    return $value;
+}
+
+/**
+ * Evaluate a particular Tempcode symbol.
+ *
+ * @ignore
+ *
+ * @param  LANGUAGE_NAME $lang The language to evaluate this symbol in (some symbols refer to language elements)
+ * @param  array $escaped Array of escaping operations
+ * @param  array $param Parameters to the symbol. For all but directive it is an array of strings. For directives it is an array of Tempcode objects. Actually there may be template-style parameters in here, as an influence of singular_bind and these may be Tempcode, but we ignore them.
+ * @return string The result
+ */
 function ecv2_DO_NOT_TRACK_REQUESTED(string $lang, array $escaped, array $param) : string
 {
     $safe = '';
