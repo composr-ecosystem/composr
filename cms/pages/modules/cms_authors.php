31132d97783e5b38687a5828c7a34a1e149df3f8
Chris Graham <chris@ocproducts.com>
2022-05-10 22:06:55 -0500

    Fix confusion merging authors

--- cms/pages/modules/cms_authors.php
+++ cms/pages/modules/cms_authors.php
@@ -435,8 +435,8 @@ class Module_cms_authors
 
         if (has_privilege(get_member(), 'delete_midrange_content')) {
             $fields = new Tempcode();
-            $fields->attach(form_input_list(do_lang_tempcode('PARAMETER_A'), '', 'mauthor', $authors));
-            $fields->attach(form_input_list(do_lang_tempcode('PARAMETER_B'), do_lang_tempcode('DESCRIPTION_NAME'), 'mauthor2', $authors));
+            $fields->attach(form_input_list(do_lang_tempcode('SOURCE'), '', 'mauthor', $authors));
+            $fields->attach(form_input_list(do_lang_tempcode('TARGET'), '', 'mauthor2', $authors));
             $post_url = build_url(['page' => '_SELF', 'type' => '_merge'], '_SELF');
             $submit_name = do_lang_tempcode('MERGE_AUTHORS');
             $merge_form = do_template('FORM', [
