490d37e132a5b0f458c895de9b80f16798beb394
Patrick Schmalstig <xanaftp@gmail.com>
2023-01-20 12:16:33 -0500

    proxy unit test WIP

--- _tests/tests/unit_tests/http.php
+++ _tests/tests/unit_tests/http.php
@@ -97,4 +97,29 @@ class http_test_set extends cms_test_case
         fclose($write);
         unlink($write_path);
     }
+
+    /*
+    public function testProxy() {
+        $old_settings = [
+            get_option('proxy'),
+            get_option('proxy_port'),
+            get_option('proxy_user'),
+            get_option('proxy_password')
+        ];
+
+        // Proxies come and go all the time; this may need to be updated.
+        set_option('proxy', '127.0.0.1', 0);
+        set_option('proxy_port', '80', 0);
+        set_option('proxy_user', '', 0);
+        set_option('proxy_password', '', 0);
+
+        $result = http_get_contents('http://example.com/', ['convert_to_internal_encoding' => true, 'trigger_error' => true]);
+        $this->assertTrue(($result !== null) && (strpos($result, 'Example Domain') !== false), 'Proxy test failed. You may need to use a different proxy.' . "\n\n" . ($result !== null ? $result : 'NULL'));
+
+        set_option('proxy', $old_settings[0], 0);
+        set_option('proxy_port', $old_settings[1], 0);
+        set_option('proxy_user', $old_settings[2], 0);
+        set_option('proxy_password', $old_settings[3], 0);
+    }
+    */
 }
