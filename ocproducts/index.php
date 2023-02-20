873c1298383df906dc83e03a31058dc7cf191bbb
Chris Graham <chris@ocproducts.com>
2023-02-19 20:21:55 -0600

    Sync changes from live server

--- ocproducts/index.php
+++ ocproducts/index.php
@@ -7,6 +7,9 @@
 
 */
 
+header('Location: https://compo.sr/');
+exit();
+
 /**
  * @license    http://opensource.org/licenses/cpal_1.0 Common Public Attribution License
  * @copyright  ocProducts Ltd
