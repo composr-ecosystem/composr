c8569b29549d65739c07c4937913720b3076cdfa
Lovinity <xanaftp@gmail.com>
2023-12-14 21:15:57 -0500

    More helpful messages for env_vars test

--- _tests/tests/unit_tests/env_vars.php
+++ _tests/tests/unit_tests/env_vars.php
@@ -50,65 +50,65 @@ class env_vars_test_set extends cms_test_case
     {
         $this->wipeData(true);
         fixup_bad_php_env_vars();
-        $this->assertTrue($_SERVER['DOCUMENT_ROOT'] == $this->bak['DOCUMENT_ROOT']);
+        $this->assertTrue($_SERVER['DOCUMENT_ROOT'] == $this->bak['DOCUMENT_ROOT'], 'DOCUMENT_ROOT is missing after wipe_data blankify');
 
         $this->wipeData(false);
         fixup_bad_php_env_vars();
-        $this->assertTrue($_SERVER['DOCUMENT_ROOT'] == $this->bak['DOCUMENT_ROOT']);
+        $this->assertTrue($_SERVER['DOCUMENT_ROOT'] == $this->bak['DOCUMENT_ROOT'], 'DOCUMENT_ROOT is missing after wipe_data non-blankify');
     }
 
     public function testMissing_PHP_SELF()
     {
         $this->wipeData(true);
         fixup_bad_php_env_vars();
-        $this->assertTrue($_SERVER['PHP_SELF'] == $this->bak['PHP_SELF']);
+        $this->assertTrue($_SERVER['PHP_SELF'] == $this->bak['PHP_SELF'], 'PHP_SELF is missing after wipe_data blankify');
 
         $this->wipeData(false);
         fixup_bad_php_env_vars();
-        $this->assertTrue($_SERVER['PHP_SELF'] == $this->bak['PHP_SELF']);
+        $this->assertTrue($_SERVER['PHP_SELF'] == $this->bak['PHP_SELF'], 'PHP_SELF is missing after wipe_data non-blankify');
     }
 
     public function testMissing_SCRIPT_FILENAME()
     {
         $this->wipeData(true);
         fixup_bad_php_env_vars();
-        $this->assertTrue($_SERVER['SCRIPT_FILENAME'] == $this->bak['SCRIPT_FILENAME']);
+        $this->assertTrue($_SERVER['SCRIPT_FILENAME'] == $this->bak['SCRIPT_FILENAME'], 'SCRIPT_FILENAME is missing after wipe_data blankify');
 
         $this->wipeData(false);
         fixup_bad_php_env_vars();
-        $this->assertTrue($_SERVER['SCRIPT_FILENAME'] == $this->bak['SCRIPT_FILENAME']);
+        $this->assertTrue($_SERVER['SCRIPT_FILENAME'] == $this->bak['SCRIPT_FILENAME'], 'SCRIPT_FILENAME is missing after wipe_data non-blankify');
     }
 
     public function testMissing_SCRIPT_NAME()
     {
         $this->wipeData(true);
         fixup_bad_php_env_vars();
-        $this->assertTrue($_SERVER['SCRIPT_NAME'] == $this->bak['SCRIPT_NAME']);
+        $this->assertTrue($_SERVER['SCRIPT_NAME'] == $this->bak['SCRIPT_NAME'], 'SCRIPT_NAME is missing after wipe_data blankify');
 
         $this->wipeData(false);
         fixup_bad_php_env_vars();
-        $this->assertTrue($_SERVER['SCRIPT_NAME'] == $this->bak['SCRIPT_NAME']);
+        $this->assertTrue($_SERVER['SCRIPT_NAME'] == $this->bak['SCRIPT_NAME'], 'SCRIPT_FILENAME is missing after wipe_data non-blankify');
     }
 
     public function testMissing_REQUEST_URI()
     {
         $this->wipeData(true);
         fixup_bad_php_env_vars();
-        $this->assertTrue($this->defaultDocNormalise($_SERVER['REQUEST_URI']) == $this->defaultDocNormalise($this->bak['REQUEST_URI']));
+        $this->assertTrue($this->defaultDocNormalise($_SERVER['REQUEST_URI']) == $this->defaultDocNormalise($this->bak['REQUEST_URI']), 'REQUEST_URI is missing after wipe_data blankify');
 
         $this->wipeData(false);
         fixup_bad_php_env_vars();
-        $this->assertTrue($this->defaultDocNormalise($_SERVER['REQUEST_URI']) == $this->defaultDocNormalise($this->bak['REQUEST_URI']));
+        $this->assertTrue($this->defaultDocNormalise($_SERVER['REQUEST_URI']) == $this->defaultDocNormalise($this->bak['REQUEST_URI']), 'REQUEST_URI is missing after wipe_data non-blankify');
     }
 
     public function testMissing_QUERY_STRING()
     {
         $this->wipeData(true);
         fixup_bad_php_env_vars();
-        $this->assertTrue($_SERVER['QUERY_STRING'] == $this->bak['QUERY_STRING']);
+        $this->assertTrue($_SERVER['QUERY_STRING'] == $this->bak['QUERY_STRING'], 'QUERY_STRING is missing after wipe_data blankify');
 
         $this->wipeData(false);
         fixup_bad_php_env_vars();
-        $this->assertTrue($_SERVER['QUERY_STRING'] == $this->bak['QUERY_STRING']);
+        $this->assertTrue($_SERVER['QUERY_STRING'] == $this->bak['QUERY_STRING'], 'QUERY_STRING is missing after wipe_data non-blankify');
     }
 }
