b072d73d9bb4ed4dbf7ab966af3b54f3c285a0c0
Lovinity <xanaftp@gmail.com>
2023-12-16 10:04:16 -0500

    PHP 8.1: Need a null check on bash parser test

--- _tests/tests/unit_tests/_bash_parser.php
+++ _tests/tests/unit_tests/_bash_parser.php
@@ -32,7 +32,7 @@ class _bash_parser_test_set extends cms_test_case
             if ((substr($c, -4) == '.php') && (basename($c) != 'errorlog.php') && (basename($c) != 'phpstub.php') && (basename($c) != 'permissioncheckslog.php')) {
                 // NB: php-no-ext bit works around bug in Windows version of PHP with slow startup. Make a ../php-no-ext/php.ini file with no extensions listed for loading
                 $message = shell_exec($php_path . ' -l ' . $c . ' -c ' . escapeshellarg(get_file_base() . '/../php-no-ext'));
-                $this->assertTrue(strpos($message, 'No syntax errors detected') !== false, $message . ' (' . $c . ')');
+                $this->assertTrue(($message !== null) && (strpos($message, 'No syntax errors detected') !== false), $message . ' (' . $c . ')');
             }
         }
     }
