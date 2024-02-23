2bb079a161efbe382f1ea1ad01c2541e0a1be562
Lovinity <xanaftp@gmail.com>
2024-02-23 16:25:15 -0500

    Allow ratchets between 4 and 31 in crypt

--- sources/crypt.php
+++ sources/crypt.php
@@ -223,7 +223,7 @@ function ratchet_hash($password, $salt, $legacy_style = 0)
 {
     if (function_exists('password_hash')) {
         // NB: We don't pass the salt separately, we let password_hash generate its own internal salt also (that builds into the hash). So it is double salted.
-        $ratchet = max(10, intval(get_option('crypt_ratchet')));
+        $ratchet = max(4, min(31, intval(get_option('crypt_ratchet'))));
         return password_hash($salt . md5($password), PASSWORD_BCRYPT, array('cost' => $ratchet));
     }
 
@@ -268,7 +268,7 @@ function produce_salt()
     if ((function_exists('openssl_random_pseudo_bytes')) && (get_value('disable_openssl') !== '1')) {
         $u = substr(md5(openssl_random_pseudo_bytes(13)), 0, 13);
     } elseif (function_exists('password_hash')) { // password_hash will include a randomised component
-        $ratchet = max(10, intval(get_option('crypt_ratchet')));
+        $ratchet = max(4, min(31, intval(get_option('crypt_ratchet'))));
         return substr(md5(password_hash(uniqid('', true), PASSWORD_BCRYPT, array('cost' => $ratchet))), 0, 13);
     } else {
         $u = substr(md5(uniqid(strval(get_secure_random_number()), true)), 0, 13);
@@ -318,7 +318,7 @@ function get_secure_random_number()
             $code = -$code;
         }
     } elseif (function_exists('password_hash')) { // password_hash will include a randomised component
-        $ratchet = max(10, intval(get_option('crypt_ratchet')));
+        $ratchet = max(4, min(31, intval(get_option('crypt_ratchet'))));
         $hash = password_hash(uniqid('', true), PASSWORD_BCRYPT, array('cost' => $ratchet));
         return crc32($hash);
     } else {
