551e33f6fd86050f3fdd39647ffcd0e716ae6ea6
Lovinity <xanaftp@gmail.com>
2024-03-06 19:20:29 -0500

    Fix build bins (unit test suite corrupted db_meta)

--- sources/version.php
+++ sources/version.php
@@ -71,7 +71,7 @@ function cms_version_number() : float
  */
 function cms_version_time() : int
 {
-    return 1708918041;
+    return 1709770706;
 }
 
 /**
@@ -82,7 +82,7 @@ function cms_version_time() : int
  */
 function cms_version_time_db() : int
 {
-    return 1708918041;
+    return 1709770706;
 }
 
 /**
