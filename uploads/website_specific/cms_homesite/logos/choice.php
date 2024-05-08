f34a8f876a240c988e8623ab0ab07615e3c9d9b9
Lovinity <xanaftp@gmail.com>
2024-05-07 22:01:33 -0400

    Fix gitignore and restore missing files

---
+++ uploads/website_specific/cms_homesite/logos/choice.php
@@ -0,0 +1,27 @@
+<?php /*
+
+ Composr
+ Copyright (c) Christopher Graham, 2004-2024
+
+ See docs/LICENSE.md for full licensing information.
+
+*/
+
+/**
+ * @license    http://opensource.org/licenses/cpal_1.0 Common Public Attribution License
+ * @copyright  Christopher Graham
+ * @package    composr_homesite
+ */
+
+$d = opendir('.');
+$one = false;
+while (($file = readdir($d)) !== false) {
+    if (substr($file, -4) == '.png') {
+        if ($one) {
+            echo ',';
+        }
+        $one = true;
+        echo $file;
+    }
+}
+closedir($d);
