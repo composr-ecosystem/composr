1820498b3936d6bb87514ae5327d4ec5a7238025
Lovinity <xanaftp@gmail.com>
2024-05-21 19:28:36 -0400

    Ignore CONFIG_CATEGORY_COMPOSR_APIS in telemetry

--- sources_custom/hooks/endpoints/cms_homesite/telemetry.php
+++ sources_custom/hooks/endpoints/cms_homesite/telemetry.php
@@ -82,9 +82,17 @@ class Hook_endpoint_cms_homesite_telemetry
                 // FUDGE: Ignore cases that might not yet be mainstream in the relay_error_notification function for the software
                 if (
                     (strpos($error_message, 'Cannot write to ') !== false) ||
-                    (strpos($error_message, 'telemetry: ') !== false)
+                    (strpos($error_message, 'telemetry: ') !== false) ||
+                    (strpos($error_message, 'CONFIG_CATEGORY_COMPOSR_APIS') !== false) // Fixed in 11.alpha4
                 ) {
                     return ['success' => true, 'error_details' => '(This relay was ignored)'];
+                }
+
+                // LEGACY: Issues fixed in 11.alpha4
+                if (
+                    (strpos($error_message, 'CONFIG_CATEGORY_COMPOSR_APIS') !== false)
+                ) {
+                    return ['success' => true, 'error_details' => 'That issue was resolved in 11.alpha4. Clear your language cache after upgrading.'];
                 }
 
                 // For our error hash, start with the full error message
