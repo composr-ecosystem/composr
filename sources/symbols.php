3f778ebf90a500a56c7bd7eeeb6d0313da49dc40
Lovinity <xanaftp@gmail.com>
2024-08-14 14:16:11 -0400

    Fixed MANTIS-5889 (CSRF tokens broken)

--- sources/symbols.php
+++ sources/symbols.php
@@ -4441,6 +4441,31 @@ function ecv_INSERT_SPAMMER_BLACKHOLE($lang, $escaped, $param)
  * @param  array $param Parameters to the symbol. For all but directive it is an array of strings. For directives it is an array of Tempcode objects. Actually there may be template-style parameters in here, as an influence of singular_bind and these may be Tempcode, but we ignore them.
  * @return string The result.
  */
+function ecv_CSRF_TOKEN($lang, $escaped, $param)
+{
+    require_code('csrf_filter');
+    $value = escape_html(generate_csrf_token());
+
+    if ($GLOBALS['XSS_DETECT']) {
+        ocp_mark_as_escaped($value);
+    }
+    if ($escaped !== array()) {
+        apply_tempcode_escaping($escaped, $value);
+    }
+
+    return $value;
+}
+
+/**
+ * Evaluate a particular Tempcode symbol.
+ *
+ * @ignore
+ *
+ * @param  LANGUAGE_NAME $lang The language to evaluate this symbol in (some symbols refer to language elements).
+ * @param  array $escaped Array of escaping operations.
+ * @param  array $param Parameters to the symbol. For all but directive it is an array of strings. For directives it is an array of Tempcode objects. Actually there may be template-style parameters in here, as an influence of singular_bind and these may be Tempcode, but we ignore them.
+ * @return string The result.
+ */
 function ecv_HONEYPOT_LINK($lang, $escaped, $param)
 {
     $value = '';
