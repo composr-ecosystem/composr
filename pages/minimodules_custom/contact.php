1cd92a546bbcf06bc941c13214d53e279c7f8003
Lovinity <xanaftp@gmail.com>
2024-09-03 16:23:26 -0400

    Updated contact module with submitting addons / themes

--- pages/minimodules_custom/contact.php
+++ pages/minimodules_custom/contact.php
@@ -49,7 +49,7 @@ if (is_guest()) {
 
 if (is_guest()) {
     $type = get_param_string('type', 'start');
-    if ($type == 'support' || $type == 'upgrade' || $type == 'installation' || $type == 'sponsor') {
+    if ($type == 'support' || $type == 'upgrade' || $type == 'installation' || $type == 'sponsor' || $type == 'addon') {
         access_denied('NOT_AS_GUEST');
     } else {
         $join_url = $GLOBALS['FORUM_DRIVER']->join_url(true);
@@ -115,6 +115,7 @@ $decision_tree = [
                     'Report an issue or request a feature',
                     'Send some general feedback',
                     'Contribute some code',
+                    'Submit a non-bundled addon or theme',
                     'Make a partnership enquiry',
                 ],
                 'options' => 'widget=radio',
@@ -128,6 +129,7 @@ $decision_tree = [
             ['free_service_type',  'Report an issue or request a feature', build_url(['page' => 'report-issue'], '')],
             ['free_service_type',  'Send some general feedback',           build_url(['page' => 'tickets', 'type' => 'ticket', 'ticket_type' => 'Feedback'], get_module_zone('tickets'))],
             ['free_service_type',  'Contribute some code',                 'contribute_code'],
+            ['free_service_type',  'Submit a non-bundled addon or theme',  'addon'],
             ['free_service_type',  'Make a partnership enquiry',           build_url(['page' => 'tickets', 'type' => 'ticket', 'ticket_type' => 'Partnership'], get_module_zone('tickets'))],
         ],
     ],
@@ -140,10 +142,11 @@ There are 3 ways to contribute code to Composr:
 [list=\"1\"]
 [*] Make a [url=\"merge request\" target=\"_blank\"]https://docs.gitlab.com/ee/user/project/merge_requests/creating_merge_requests.html[/url] on [url=\"GitLab\" target=\"_blank\"]" . CMS_REPOS_URL . "[/url]. It is also a good idea to [url=\"create an issue on the tracker\" target=\"_blank\"]" . get_base_url() . "/tracker/bug_report_page.php[/url] to reference your changes and merge request.
 [*] Post a patch [url=\"in an issue on the tracker\" target=\"_blank\"]" . get_base_url() . "/tracker/bug_report_page.php[/url].
+[*] Make a non-bundled theme or addon. For this, please see our [page=\":addon_submission\"]addon submission guidelines[/page].
 [/list]
 For contributions to any of the bundled addons we may need a standard dual-copyright agreement signing. We'll get in touch regarding that if necessary.
 
-Also ask us if you want to be listed as one of the [page=\"site:stars\"]Composr developers[/page].",
+Also ask us if you want to be listed as one of the [page=\"site:stars\"]Composr developers[/page] after making your contribution.",
         'previous' => 'free',
     ],
 
@@ -153,6 +156,48 @@ Also ask us if you want to be listed as one of the [page=\"site:stars\"]Composr 
         'previous' => 'start',
         'next' => build_url(['page' => 'partners'], ''),
         'form_method' => 'GET',
+    ],
+
+    'addon' => [
+        'title' => 'Submit non-bundled addon or theme via Support Ticket',
+        'text' => "Great! We would love to promote your creation. If you have not already done so yet, please review the [page=\":addon_submission\"]addon submission guidelines[/page]. Once you have, use this form to submit your addon or theme. You can also use this form to submit an update to an addon or theme you already submitted (though we much prefer you make a merge request on our GitLab instead; your addon will be there if it was accepted).",
+        'previous' => 'free',
+        'form_method' => 'POST',
+        'notice' => [
+            'This form will create a Support Ticket between you and the core team. They will then review your submission and either approve it or reject it (with their reasoning and some suggestions on what you can do next).'
+        ],
+        'questions' => [
+            'title' => [
+                'label' => 'Addon / Theme Name',
+                'description' => 'What is the name of your addon or theme?',
+                'type' => 'short_text',
+                'default' => '',
+                'required' => true,
+            ],
+            'type' => [
+                'label' => 'Type',
+                'description' => 'What type of submission is this?',
+                'type' => 'list',
+                'default' => 'New Addon',
+                'default_list' => [
+                    'New Addon',
+                    'Update to an Addon',
+                    'New Theme',
+                    'Update to a Theme',
+                ],
+                'options' => 'widget=radio',
+                'required' => true,
+            ],
+            'addon_file' => [
+                'label' => 'Addon File',
+                'description' => 'Please upload your addon archive here. The core team prefers you use the export addon tool in Composr; it is the easiest method both for you and for us. You can alternatively use an archive of your files (but make sure you have a sources_custom/hooks/systems/addon_registry file).',
+                'type' => 'upload',
+                'default' => '',
+                'required' => true,
+            ],
+        ],
+        'needs_captcha' => ((addon_installed('captcha')) && (get_option('captcha_on_feedback') == '1') && (use_captcha())),
+        'next' => build_url(['page' => 'tickets', 'type' => 'post', 'ticket_type' => 'Addon / Theme Submission'], get_module_zone('tickets')),
     ],
 ];
 
