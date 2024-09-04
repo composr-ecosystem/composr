557bbd402fafcda6b32bb1dc2ce19c7b3eec416a
Lovinity <xanaftp@gmail.com>
2024-09-04 18:06:29 -0400

    Reduce pagination max default to 10 on notifications

--- site/pages/modules/notifications.php
+++ site/pages/modules/notifications.php
@@ -143,7 +143,7 @@ class Module_notifications
     public function browse() : object
     {
         $start = get_param_integer('n_start', 0);
-        $max = get_param_integer('n_max', 50);
+        $max = get_param_integer('n_max', 10); // We keep this low by default or we may trigger PHP memory errors
 
         require_code('notification_poller');
         list($notifications, $max_rows) = get_web_notifications($max, $start);
