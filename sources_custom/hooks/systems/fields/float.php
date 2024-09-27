3ef4b0f47a6b074de29dbd1e3d41e06682ac7af4
Lovinity <xanaftp@gmail.com>
2024-09-27 16:05:13 -0400

    Additional tutorial work (WIP)

--- sources_custom/hooks/systems/fields/float.php
+++ sources_custom/hooks/systems/fields/float.php
@@ -267,4 +267,16 @@ class Hook_fields_float
     {
         return '';
     }
+
+    /**
+     * Define what type of field this should be treated as in the privacy system if marked sensitive.
+     * This method should be defined on fields which should not be treated as "additional_anonymise_fields".
+     *
+     * @param  array $field The field details
+     * @return ID_TEXT The type of field to treat this
+     */
+    public function privacy_field_type(array $field) : string
+    {
+        return 'number_field_anonymise_only';
+    }
 }
