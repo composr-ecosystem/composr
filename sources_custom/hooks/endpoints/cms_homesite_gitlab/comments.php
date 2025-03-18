cb0d6556e370b50684225463abac3e8ee1cb7b24
Lovinity <xanaftp@gmail.com>
2025-03-17 21:10:34 -0400

    #4 - GitLab Comments Integration (untested)

---
+++ sources_custom/hooks/endpoints/cms_homesite_gitlab/comments.php
@@ -0,0 +1,112 @@
+<?php /*
+
+ Composr
+ Copyright (c) Christopher Graham, 2004-2024
+
+ See docs/LICENSE.md for full licensing information.
+
+*/
+
+/**
+ * @license    http://opensource.org/licenses/cpal_1.0 Common Public Attribution License
+ * @copyright  Christopher Graham
+ * @package    cms_homesite_gitlab
+ */
+
+/**
+ * Hook class.
+ */
+class Hook_endpoint_cms_homesite_gitlab_comments
+{
+    /**
+     * Return information about this endpoint.
+     *
+     * @param  ?string $type Standard type parameter, usually either of add/edit/delete/view (null: not-set)
+     * @param  ?string $id Standard ID parameter (null: not-set)
+     * @return ?array Info about the hook (null: endpoint is disabled)
+     */
+    public function info(?string $type, ?string $id) : ?array
+    {
+        if (!addon_installed('cms_homesite')) {
+            return null;
+        }
+        if (!addon_installed('cms_homesite_gitlab')) {
+            return null;
+        }
+        if (!addon_installed('points')) {
+            return null;
+        }
+
+        return [
+            'authorization' => ['gitlab_webhook'],
+            'log_stats_event' => 'cms_homesite_gitlab/comments',
+        ];
+    }
+
+    /**
+     * Run an API endpoint.
+     *
+     * @param  ?string $type Standard type parameter, usually either of add/edit/delete/view (null: not-set)
+     * @param  ?string $id Standard ID parameter (null: not-set)
+     * @return array Data structure that will be converted to correct response type
+     */
+    public function run(?string $type, ?string $id) : array
+    {
+        // The JSON payload was coerced by the main endpoints script
+        $data = json_decode($_POST['data'], true);
+
+        // Integrity check
+        if (!isset($data['object_kind']) || !isset($data['object_attributes']['action'])) {
+            return ['success' => false, 'error_details' => 'Invalid webhook data.'];
+        }
+
+        // We only award points for new comments
+        if (!in_array($data['object_attributes']['action'], ['create'])) {
+            return ['success' => false, 'error_details' => 'Only actionable on creating comments.'];
+        }
+
+        // Try figuring out which of our members opened the issue on GitLab
+        $member_id = null;
+
+        // Maximum authenticity: Hybridauth linked GitLab account
+        if (($member_id === null) && (isset($data['user']['id']))) {
+            $member_id = $GLOBALS['FORUM_DB']->query_select_value_if_there('f_members', 'id', ['m_password_compat_scheme' => 'GitLab', 'm_pass_hash_salted' => strval($data['user']['id'])], 'ORDER BY m_join_time DESC,id DESC');
+        }
+
+        // Medium authenticity: matching e-mail address
+        if (($member_id === null) && (isset($data['user']['email']))) {
+            $member_id = $GLOBALS['FORUM_DRIVER']->get_member_from_email_address($data['user']['email']);
+        }
+
+        // Low authenticity: matching username
+        if (($member_id === null) && (isset($data['user']['username']))) {
+            $member_id = $GLOBALS['FORUM_DRIVER']->get_member_from_username($data['user']['username']);
+        }
+
+        // Could not find any members? Exit out.
+        if ($member_id === null) {
+            return ['success' => false, 'error_details' => 'No member found matching the GitLab user responsible for the comment action.'];
+        }
+
+        $points = intval(get_option('gitlab_comment_points'));
+
+        // Ignored if not awarding points for any issues
+        if ($points < 0) {
+            return ['success' => false, 'error_details' => 'Ignored; no points are to be awarded.'];
+        }
+
+        require_code('points2');
+        require_code('templates');
+        require_lang('cms_homesite_gitlab');
+
+        $_reason = do_lang('UNKNOWN');
+        if (isset($data['object_attributes']['noteable_type'])) {
+            $data['object_attributes']['noteable_type'];
+        }
+        $reason = do_lang('POINTS_GITLAB_CREATED_COMMENT', comcode_escape(strval($data['object_attributes']['id'])), comcode_escape($_reason));
+
+        points_credit_member($member_id, $reason, $points, 0, true, 0, 'gitlab', 'comment', strval($data['object_attributes']['id']));
+
+        return ['success' => true];
+    }
+}
---
+++ sources_custom/hooks/systems/points/gitlab__comment.php
@@ -0,0 +1,39 @@
+<?php /*
+
+ Composr
+ Copyright (c) Christopher Graham, 2004-2024
+
+ See docs/LICENSE.md for full licensing information.
+
+*/
+
+/**
+ * @license    http://opensource.org/licenses/cpal_1.0 Common Public Attribution License
+ * @copyright  Christopher Graham
+ * @package    cms_homesite_gitlab
+ */
+
+/**
+ * Hook class.
+ * Points hooks should follow this naming convention according to points_ledger column names: t_type__t_subtype (or just t_type to match all t_subtypes). By defining a hook accordingly, you are indicating that all transactions matching the t_type (and t_subtype if applicable) are considered 'low-impact'. This means they will be hidden from ledger tables on members' point profiles and instead displayed as a cumulative tally (determined by the points_profile function) at the top of their points profile.
+ */
+class Hook_points_gitlab__comment
+{
+    /**
+     * Determine the aggregate row language for POINTS_PROFILE.tpl.
+     *
+     * @param  ?MEMBER $member_id_of The ID of the member who is being viewed (null: was run from the admin ledger)
+     * @param  ?MEMBER $member_id_viewing The ID of the member who is doing the viewing (null: current member)
+     * @return ?array List containing label for use with aggregate point tables (null: hook disabled)
+     */
+    public function points_profile(?int $member_id_of, ?int $member_id_viewing) : ?array
+    {
+        if (!addon_installed('cms_homesite_gitlab')) {
+            return null;
+        }
+
+        return [
+            'label' => do_lang('cms_homesite_gitlab:GITLAB_COMMENT_POINTS'),
+        ];
+    }
+}
