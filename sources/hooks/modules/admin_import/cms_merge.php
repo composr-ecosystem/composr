e57d5e3bf67a0726d711834547b5db3785ce070d
Lovinity <xanaftp@gmail.com>
2025-03-24 15:47:46 -0400

    Override feedback dependencies for composr.app import

--- sources/hooks/modules/admin_import/cms_merge.php
+++ sources/hooks/modules/admin_import/cms_merge.php
@@ -113,7 +113,7 @@ class Hook_import_cms_merge
             // This dependency tree is overdefined, but I wanted to make it clear what depends on what, rather than having a simplified version
             'attachment_references' => ['attachments', 'cns_members', 'cns_posts', 'news_and_categories', 'wiki'],
             'permissions' => array_diff($info['import'], ['feedback', 'attachment_references', 'permissions', 'stats', 'config', 'menu_items', 'redirects']),
-            'feedback' => array_diff($info['import'], ['themes', 'cns_warnings', 'feedback', 'attachment_references', 'permissions', 'stats', 'config', 'menu_items', 'redirects']),
+            //'feedback' => array_diff($info['import'], ['themes', 'cns_warnings', 'feedback', 'attachment_references', 'permissions', 'stats', 'config', 'menu_items', 'redirects']),
             'authors' => ['cns_members', 'catalogues'],
             'banners' => ['cns_members'],
             'catalogues' => ['cns_members'],
