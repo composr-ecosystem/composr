f255188f354971be4e6e7c4451d895b1fa8d7c2d
Lovinity <xanaftp@gmail.com>
2025-07-30 14:36:34 -0400

    Enhance persistent_cache test; fix caches

--- _tests/tests/sync_tests/persistent_cache.php
+++ _tests/tests/sync_tests/persistent_cache.php
@@ -100,4 +100,60 @@ class persistent_cache_test_set extends cms_test_case
         $got = $cache->get('test', $time + 1);
         $this->assertTrue($got === null);
     }
+
+    public function testMainCache()
+    {
+        cms_extend_time_limit(75);
+
+        require_code('tempcode');
+        require_code('lorem');
+        require_code('caches');
+        require_code('caches2');
+
+        $tempcode_empty = new Tempcode();
+        $tempcode_not_empty = do_lorem_template('PARAGRAPH', [
+            'TEXT' => lorem_sentence_html(),
+            'CLASS' => lorem_phrase(),
+        ]);
+
+        // Try strings, integers, floats, arrays, objects, and Tempcode.
+        $values = [uniqid('', false), '', mt_rand(0, 1000000), pi(), ['foo' => 'bar'], new dummy_class(), $tempcode_empty, $tempcode_not_empty];
+
+        // Set cache entries
+        foreach ($values as $key => $value) {
+            set_cache_entry('test', 1, strval($key), $value, CACHE_AGAINST_DEFAULT, [], [], [], ($key >= (count($values) - 2)));
+        }
+
+        // Every 16 seconds for 4 iterations, try to get the value (and we expect it to fetch)
+        for ($i = 0; $i < 4; $i++) {
+            foreach ($values as $key => $value) {
+                $ret = get_cache_entry('test', strval($key), CACHE_AGAINST_DEFAULT, 1, ($key >= (count($values) - 2)));
+                $this->assertTrue(($ret !== null), 'Expected cache key ' . strval($key) . ' to return a value but it did not.');
+
+                if ($ret !== null) {
+                    if ($key >= (count($values) - 2)) { // Tempcode
+                        $this->assertTrue(($ret->evaluate() == $value->evaluate()), 'Expected cache key '. strval($key) . ' to be ' . $value->evaluate() . ' but instead it was ' . $ret->evaluate());
+                    } else {
+                        $this->assertTrue((serialize($ret) == serialize($value)), 'Expected cache key '. strval($key) . ' to be ' . serialize($value) . ' but instead it was ' . serialize($ret));
+                    }
+                }
+            }
+            sleep(16);
+        }
+
+        // Now, over a minute passed, so we expect cache misses
+        foreach ($values as $key => $value) {
+            $ret = get_cache_entry('test', strval($key), CACHE_AGAINST_DEFAULT, 1);
+            $this->assertTrue(($ret === null), 'Expected cache key ' . strval($key) . ' to have expired, but it did not.');
+        }
+    }
+}
+
+class dummy_class
+{
+    protected $some_value;
+    public function __construct()
+    {
+        $this->some_value = 0;
+    }
 }
